1caa4e2d788e94cac648088695aef225eb858834
pure90719@gmail.com <pure90719@gmail.com>
2022-11-22 13:28:31 +0800

    vite

---
+++ src/components/headerComponents.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+    <nav>
+        <router-link class="anchor" to="/">Home</router-link>
+        <router-link class="anchor" to="/about">About</router-link>
+        <router-link class="anchor" to="/news">News</router-link>
+        <router-link class="anchor" to="/shop">Shop</router-link>
+        <router-link class="anchor" to="/shop/shopInfo">shopInfo</router-link>
+        <router-link class="anchor" to="/cart">Cart</router-link>
+        <router-link class="anchor" to="/game">Game</router-link>
+        <router-link class="anchor" to="/custom">Custom</router-link>
+        <router-link class="anchor" to="/showcase">Showcase</router-link>
+        <router-link class="anchor" to="/race">Race</router-link>
+        <router-link class="anchor" to="/member">Member</router-link>
+    </nav>
+    <router-view />
+</template>
+
+<style lang="scss" scoped>
+  nav{
+    width: 100%;
+    height: 60px;
+    background: #000;
+    .anchor{
+      color: #fff;
+      display: inline-block;
+      margin: 10px;
+    }
+  }
+</style>
---
+++ src/views/shopView.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+    <h1>shop</h1>
+</template>
+
+<style>
+
+</style>
