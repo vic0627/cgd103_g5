55b379ca1b87f5577bc57bfada8e2e68024d7594
pure90719@gmail.com <pure90719@gmail.com>
2022-11-23 14:22:58 +0800

    fix main.js , btn and mouse examples

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import "./css/reset.css"
+import '@/sass/style.scss'
 
 const app = createApp(App)
 
--- src/views/shopInfoView.vue
+++ src/views/shopInfoView.vue
@@ -2,22 +2,27 @@
 import { onMounted } from 'vue';
 import mouseComponentsVue from '../components/mouseComponents.vue';
 import * as MOUSE from '@/composables/useMouseMove';
-
 onMounted(()=> {
     MOUSE.useMouseMove();
-    window.addEventListener('click', ()=> {
-        MOUSE.mouseFocus();
-    })
-    window.addEventListener('keydown', ()=> {
-        MOUSE.mouseRe();
-    })
 })
 </script>
 
 <template>
+    <h1>Shop Info</h1>
+    <a
+        href="#"
+        class="btn"
+        data-title="buy now"
+        @mouseenter="MOUSE.mouseFocus()"
+        @mouseleave="MOUSE.mouseRe()"
+    >buy now</a>
     <mouseComponentsVue />
 </template>
 
 <style lang="scss" scoped>
-
+@import '@/sass/style.scss';
+.btn{
+    @include primaryBtn(120px);
+    margin: 40px;
+}
 </style>
