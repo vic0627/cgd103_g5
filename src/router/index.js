623ed13d78cb808504c9909a3ea2873538764f57
LilyLiuweb <phliu814@gmail.com>
2022-11-25 20:44:21 +0800

    Merge branch 'dev' into Lily
    # Conflicts:
    #	src/css/style.css.map

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,6 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/news',
       name: 'news',
       component:  () => import('../views/newsView.vue')
