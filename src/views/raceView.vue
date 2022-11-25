b2f908d5bb5d7ea9b0bd9f6b7cd14953d363e766
YiHsuan8 <ftyhnbv243@gmail.com>
2022-11-26 00:56:17 +0800

    add image,backend

--- src/views/raceView.vue
+++ src/views/raceView.vue
@@ -1,11 +1,19 @@
-<script>
-
-</script>
+<script setup></script>
 
 <template>
+  <!-- 內容 -->
 
+  <main class="wrapper">
+    <!-- banner -->
+    <div class="banner">
+      <div class="banner_pic"></div>
+    </div>
+  </main>
 </template>
 
-<style>
+<style lang="scss" scoped>
+// banner
 
+.banner {
+}
 </style>
