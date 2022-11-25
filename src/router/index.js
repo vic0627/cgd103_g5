beb14721316f38d6dbf89aace08da2bc9be81f8a
a31614738 <a0967331773@gmail.com>
2022-11-25 17:09:41 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'landing',
-      component: () => import('../views/LandingView.vue')
-    },
+    // {
+    //   path: '/',
+    //   name: 'landing',
+    //   component: () => import('../views/LandingView.vue')
+    // },
     {
       path: '/home',
       name: 'home',
