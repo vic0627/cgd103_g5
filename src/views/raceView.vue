44aef04825771a357f3e526642ebba9136fbe3d1
YiHsuan8 <ftyhnbv243@gmail.com>
2022-11-27 15:57:27 +0800

    add race

--- src/views/raceView.vue
+++ src/views/raceView.vue
@@ -4,20 +4,20 @@
   <main>
     <!-- banner -->
     <section class="banner">
-      <div class="test">
-        <!-- <img src="../assets/images/race/banner01.jpg" alt="banner" /> -->
+      <div>
+        <img src="../assets/images/race/banner01.jpg" alt="banner" />
       </div>
 
       <div>
-        <!-- <img src="../assets/images/race/banner02.jpg" alt="banner" /> -->
+        <img src="../assets/images/race/banner02.jpg" alt="banner" />
       </div>
 
       <div>
-        <!-- <img src="../assets/images/race/banner03.jpg" alt="banner" /> -->
+        <img src="../assets/images/race/banner03.jpg" alt="banner" />
       </div>
 
       <div>
-        <!-- <img src="../assets/images/race/banner04.jpg" alt="banner" /> -->
+        <img src="../assets/images/race/banner04.jpg" alt="banner" />
       </div>
     </section>
 
@@ -30,22 +30,21 @@
 <style lang="scss" scoped>
 @import "../sass/style.scss";
 @import "../sass/component/_btn.scss";
-.test {
-  background-image: url(../assets/images/race/banner01.jpg);
-}
-section {
-  margin: 0;
-  //banner
-  .banner {
-    width: 100vw;
-    height: 650px;
-    overflow: hidden;
-    position: relative;
-    background-color: black;
-    dev {
-      width: 100%;
-      background-position: center center;
-    }
+
+.banner {
+  margin: 30px auto;
+  width: 100%;
+  //   height: 60px;
+  position: relative;
+  box-shadow: #666 0 0 5px;
+  dev {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    filter: alpha(opacity=0);
+    -webkit-animation: round 25s linear infinite;
+    animation: round 25s linear infinite;
   }
 }
 </style>
