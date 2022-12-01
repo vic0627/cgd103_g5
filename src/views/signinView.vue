992f5501468e3423e9fa1b9832cb3df7083337ef
Nozomi <>
2022-12-01 20:28:45 +0800

    2028

--- src/views/signinView.vue
+++ src/views/signinView.vue
@@ -97,7 +97,7 @@
     position: relative;
     width: 100%;
     .img_box{
-        background-image: url("../assets/images/Signin/bgimg01.jpeg");
+        background-image: url("../assets/images/Signin/pexels-tembela-bohle-2050718.jpeg");
         position: fixed;
         top: 0;
         left: 0;
@@ -114,7 +114,7 @@
             margin-left: auto;
             position: relative;
             width: 400px;
-            background-color: #fff;
+            background-color: rgb(28, 30, 57);
             box-shadow: 0 16px 32px rgb(0 0 0 / 10%);
             padding: 48px 32px;
             .logo{
