c38f0578d371db0bc6117b0cdaf3ca5853feebf2
YiHsuan8 <ftyhnbv243@gmail.com>
2022-12-01 21:15:21 +0800

    new branch

--- src/views/raceView.vue
+++ src/views/raceView.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 
 <template>
-  <main>
-    <!-- banner -->
+  <!-- banner -->
+  <section>
     <div class="banner">
       <div class="banner_img">
         <img src="../assets/images/race/banner01.jpg" alt="banner" />
@@ -23,40 +23,501 @@
       <div class="banner_title">
         <h2>CONTEST</h2>
       </div>
+    </div>
+  </section>
 
-      <div class="test"></div>
+  <!-- race list -->
+
+  <section class="recentMatches">
+    <div class="raceList_title">
+      <h2>RECENT MATCHES</h2>
     </div>
 
-    <!-- race list -->
+    <div class="raceList">
+      <div class="raceBox raceBox_w">
+        <div class="bxoBgi_w"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_w">World</span>
+          </div>
 
-    <section class="raceList">
-      <div class="box">
-        <p>123</p>
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
       </div>
-    </section>
-    <!-- background img -->
-    <section class="backgroundImage"></section>
-    <!-- practise point -->
-    <section class="practisePoint"></section>
-  </main>
+      <div class="raceBox raceBox_t">
+        <div class="bxoBgi_t"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_t">Taiwan</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="raceBox raceBox_w">
+        <div class="bxoBgi_w"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_w">World</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="raceBox raceBox_t">
+        <div class="bxoBgi_t"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_t">Taiwan</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="raceBox raceBox_w">
+        <div class="bxoBgi_w"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_w">World</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="raceBox raceBox_t">
+        <div class="bxoBgi_t"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_t">Taiwan</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="raceBox raceBox_w">
+        <div class="bxoBgi_w"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_w">World</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="raceBox raceBox_t">
+        <div class="bxoBgi_t"></div>
+        <div class="listWord">
+          <div class="nation">
+            <span class="nation_t">Taiwan</span>
+          </div>
+
+          <div class="date">
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>01</span>
+
+            <br />
+
+            <span>|</span>
+
+            <br />
+
+            <span>2022</span>
+
+            <span>01</span>
+
+            <span>10</span>
+          </div>
+
+          <div class="competitionName">
+            <h4>competition name</h4>
+          </div>
+
+          <div class="learn">
+            <a
+              href="#"
+              class="btn"
+              id="btn1"
+              data-title="Learn"
+              @mouseenter="MOUSE.mouseFocus()"
+              @mouseleave="MOUSE.mouseRe()"
+            >
+              <span>Learn</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- background img -->
+  <section class="backgroundImage">
+    <div class="image"></div>
+  </section>
+  <!-- Aerial point  -->
+  <section class="aerialPoint">
+    <div class="aerialPoint_title">
+      <h2>Aerial Point</h2>
+    </div>
+
+    <div class="pointBox">
+      <div class="bigtext"></div>
+
+      <div class="practiseCard">
+        <div class="card card1">
+          <div class="cardTitle">
+            <h2>Taiwan</h2>
+          </div>
+
+          <div class="cardPic">
+            <img src="../assets/images/race/point1.jpg" alt="aerialPoint" />
+          </div>
+
+          <div class="cardContent">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+              itaque. Modi officia sed cupiditate autem, beatae perspiciatis
+              consectetur accusantium aliquam in voluptates distinctio.
+              Deleniti, illum? Expedita corrupti perspiciatis sunt fugiat!
+              Accusamus sit illo, expedita sequi reiciendis laudantium soluta.
+              Ullam veritatis dignissimos voluptatum corporis dolor dolores nisi
+              maiores earum autem beatae.
+            </p>
+          </div>
+        </div>
+
+        <div class="card card2">
+          <div class="cardTitle">
+            <h2>Taiwan</h2>
+          </div>
+
+          <div class="cardPic">
+            <img src="../assets/images/race/point2.jpg" alt="aerialPoint" />
+          </div>
+
+          <div class="cardContent">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+              itaque. Modi officia sed cupiditate autem, beatae perspiciatis
+              consectetur accusantium aliquam in voluptates distinctio.
+              Deleniti, illum? Expedita corrupti perspiciatis sunt fugiat!
+              Accusamus sit illo, expedita sequi reiciendis laudantium soluta.
+              Ullam veritatis dignissimos voluptatum corporis dolor dolores nisi
+              maiores earum autem beatae.
+            </p>
+          </div>
+        </div>
+
+        <div class="card card3">
+          <div class="cardTitle">
+            <h2>Taiwan</h2>
+          </div>
+
+          <div class="cardPic">
+            <img src="../assets/images/race/point3.jpg" alt="aerialPoint" />
+          </div>
+
+          <div class="cardContent">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+              itaque. Modi officia sed cupiditate autem, beatae perspiciatis
+              consectetur accusantium aliquam in voluptates distinctio.
+              Deleniti, illum? Expedita corrupti perspiciatis sunt fugiat!
+              Accusamus sit illo, expedita sequi reiciendis laudantium soluta.
+              Ullam veritatis dignissimos voluptatum corporis dolor dolores nisi
+              maiores earum autem beatae.
+            </p>
+          </div>
+        </div>
+
+        <div class="card card4">
+          <div class="cardTitle">
+            <h2>Taiwan</h2>
+          </div>
+
+          <div class="cardPic">
+            <img src="../assets/images/race/point4.jpg" alt="aerialPoint" />
+          </div>
+
+          <div class="cardContent">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+              itaque. Modi officia sed cupiditate autem, beatae perspiciatis
+              consectetur accusantium aliquam in voluptates distinctio.
+              Deleniti, illum? Expedita corrupti perspiciatis sunt fugiat!
+              Accusamus sit illo, expedita sequi reiciendis laudantium soluta.
+              Ullam veritatis dignissimos voluptatum corporis dolor dolores nisi
+              maiores earum autem beatae.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
 @import "../sass/style.scss";
 @import "../sass/component/_btn.scss";
-section {
-  margin: 0;
-}
 
 // banner
+
 .banner {
   margin: 0;
+  width: 100%;
+  height: 176px;
   position: relative;
-  // position: absolute;
+  @include l($l-breakpoint) {
+    height: 760px;
+  }
   .banner_img {
     width: 100%;
-    top: 0;
-    left: 0;
     opacity: 0;
     filter: alpha(opacity=0);
     -webkit-animation: round 20s linear infinite;
@@ -64,14 +525,13 @@ section {
     img {
       width: 100%;
       position: absolute;
-      //   position: relative;
     }
   }
   .banner_title {
     position: absolute;
-    top: 5%;
+    top: 50px;
     left: 50%;
-    transform: translate(-50%, 0%);
+    transform: translate(-50%, -50%);
   }
 }
 @-webkit-keyframes round {
@@ -79,10 +539,12 @@ section {
     opacity: 1;
     filter: alpha(opacity=100);
   }
+
   25% {
     opacity: 1;
     filter: alpha(opacity=100);
   }
+
   50% {
     opacity: 0;
     filter: alpha(opacity=0);
@@ -93,33 +555,180 @@ section {
     opacity: 1;
     filter: alpha(opacity=100);
   }
+
   25% {
     opacity: 1;
     filter: alpha(opacity=100);
   }
+
   50% {
     opacity: 0;
     filter: alpha(opacity=0);
   }
 }
+
 .banner_img:nth-child(1) {
   -webkit-animation-delay: 0s;
   animation-delay: 0s;
 }
+
 .banner_img:nth-child(2) {
   -webkit-animation-delay: 5s;
   animation-delay: 5s;
 }
+
 .banner_img:nth-child(3) {
   -webkit-animation-delay: 10s;
   animation-delay: 10s;
 }
+
 .banner_img:nth-child(4) {
   -webkit-animation-delay: 15s;
   animation-delay: 15s;
 }
 
-//background img
+//race list
+
+.raceList_title {
+  text-align: center;
+  padding-bottom: 50px;
+}
+
+.raceList {
+  max-width: 100%;
+  height: 600px;
+  margin: 0 20px;
+  display: flex;
+  overflow: scroll;
+  @include l($l-breakpoint) {
+    overflow: visible;
+    &:hover {
+      animation-play-state: paused;
+    }
+    animation: scroll 0.1s linear infinite;
+    @keyframes scroll {
+      0% {
+        transform: translateX(0);
+      }
+      100% {
+        transform: translateX(calc(-300px * 2));
+      }
+    }
+  }
+  .raceBox {
+    min-width: 100%;
+    position: relative;
+    @include l($l-breakpoint) {
+      min-width: 300px;
+    }
+    .bxoBgi_w {
+      background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)),
+        url("../assets/images/race/contest01.png");
+      backdrop-filter: blur(5px);
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      background-position: center center;
+    }
+    .bxoBgi_t {
+      background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)),
+        url("../assets/images/race/place01.png");
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      background-position: center center;
+    }
+    .listWord {
+      min-width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      text-align: center;
+      .nation {
+        .nation_t {
+          color: $brown;
+        }
+
+        .nation_w {
+          color: $ored;
+        }
+        .nation_t,
+        .nation_w {
+          top: -10px;
+          font-size: 40px;
+          line-height: 5rem;
+          font-weight: bold;
+        }
+      }
+      span {
+        // display: block;
+        font-size: 30px;
+        line-height: 4rem;
+        font-weight: bold;
+      }
+      h4 {
+        font-size: 40px;
+        margin: 35px 0;
+        font-weight: bold;
+      }
+      .learn {
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }
+}
+
+// background img
+
 .backgroundImage {
+  width: 100%;
+  height: 600px;
+  .image {
+    background-image: url("../assets/images/race/background image.png");
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center center;
+    background-attachment: fixed;
+  }
+}
+
+//Aerial point
+
+.aerialPoint_title {
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.pointBox {
+  display: flex;
+  .bigtext {
+    width: 460px;
+    height: 700px;
+    border: 1px solid red;
+    margin: 0 20px;
+    display: none;
+  }
+  .practiseCard {
+    // display: flex;
+    gap: 30px;
+    margin: 0 20px;
+    overflow: scroll;
+    .card {
+      min-width: 100%;
+      height: 550px;
+      border: 1px solid red;
+      padding: 20px;
+      border-radius: 20px;
+      .cardPic {
+        width: 300px;
+        img {
+          width: 100%;
+          margin: 20px 0;
+        }
+      }
+    }
+  }
 }
 </style>
