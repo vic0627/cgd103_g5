395a0a9b1b73935a535ef329ac17df391aa970bd
Nozomi <>
2022-12-05 16:53:49 +0800

    12051653

--- src/views/CustomizedOrders.vue
+++ src/views/CustomizedOrders.vue
@@ -4,6 +4,7 @@ import { ref, onMounted, createApp } from 'vue';
 
 
 
+
 const aa = ref({
     name:"hihi",
     age : ["apple","water"],
@@ -11,6 +12,22 @@ const aa = ref({
 });
 console.log("aa:",aa.value.name);
 
+const nestedData = ref({
+  product: {
+    M1: "Macbook Air M1 2020",
+    M2: "Macbook Air M2 2022",
+  },
+  retina: {
+    M1: "13.3 吋",
+    M2: "13.6 吋",
+  },
+  chips: {
+    M1: "Apple M1 晶片",
+    M2: "Apple M2 晶片",
+  },
+});
+
+const greeting = ref("123123");
 
 
 const state = ref({
@@ -35,8 +52,23 @@ onMounted(()=> {
     <headerComponentsVue />
     <div>{{aa.name}}</div>
     <div>{{aa.age[0]}}</div>
+    <div>{{greeting}}</div>
 
     <button @click="increment">{{ state.count }}</button>
+
+
+    <div class="wrapper">
+        <div
+            v-for="(categoryContent, categoryName) in nestedData"
+            :key="categoryName"
+            
+        >
+            <p>{{ categoryName }}</p>
+            <div v-for="(content, chip) in categoryContent" :key="content">
+                <p>{{ content }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 
@@ -49,3 +81,6 @@ onMounted(()=> {
 }
 
 </style>
+
+
+
