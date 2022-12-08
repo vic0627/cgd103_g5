2c7dc4327e296a8ed971e91a7dd656f423f760fc
Ramiel2209 <sa23457848551836@gmail.com>
2022-12-08 09:15:41 +0800

    ../

--- src/views/shopInfo2View.vue
+++ src/views/shopInfo2View.vue
@@ -107,25 +107,22 @@ const btnLeft = () => {
     <div class="colorSwitch">
       <div class="colorSwitch_photo">
         <div class="color_img">
-          <img src="src/assets/images/shopInfo/body_03_2.png" alt="color" />
+          <img src="../assets/images/shopInfo/body_03_2.png" alt="color" />
+        </div>
+
+        <div class="color_img">
+          <img src="../assets/images/shopInfo/body_03_blue_2.png" alt="color" />
         </div>
 
         <div class="color_img">
           <img
-            src="src/assets/images/shopInfo/body_03_blue_2.png"
+            src="../assets/images/shopInfo/body_03_white_2.png"
             alt="color"
           />
         </div>
 
         <div class="color_img">
-          <img
-            src="src/assets/images/shopInfo/body_03_white_2.png"
-            alt="color"
-          />
-        </div>
-
-        <div class="color_img">
-          <img src="src/assets/images/shopInfo/body_03_red_2.png" alt="color" />
+          <img src="../assets/images/shopInfo/body_03_red_2.png" alt="color" />
         </div>
       </div>
       <div class="colorSwitch_word">
@@ -139,7 +136,7 @@ const btnLeft = () => {
     </div>
 
     <div class="battery">
-      <img src="src/assets/images/shopInfo/battery.jpg" alt="battery" />
+      <img src="../assets/images/shopInfo/battery.jpg" alt="battery" />
       <h4>46-Min Extended Flight Time</h4>
       <p>
         Increasing flight time by almost 50% compared to Mavic 2 Pro, Mavic 3
@@ -148,7 +145,7 @@ const btnLeft = () => {
     </div>
 
     <div class="far">
-      <img src="src/assets/images/shopInfo/far.jpg" alt="far" />
+      <img src="../assets/images/shopInfo/far.jpg" alt="far" />
       <h4>O3+ Transmission, 15km Range</h4>
       <p>
         With a transmission distance of up to 15 km, fly farther and more stably
@@ -157,7 +154,7 @@ const btnLeft = () => {
     </div>
 
     <div class="accessories">
-      <img src="src/assets/images/shopInfo/accessories.jpg" alt="accessories" />
+      <img src="../assets/images/shopInfo/accessories.jpg" alt="accessories" />
       <h4>Extensive Accessories</h4>
       <p>
         DJI RC features a built-in 5.5-inch HD display for clear viewing in
@@ -167,7 +164,7 @@ const btnLeft = () => {
     </div>
 
     <div class="sensing">
-      <img src="src/assets/images/shopInfo/sensing.jpg" alt="sensing" />
+      <img src="../assets/images/shopInfo/sensing.jpg" alt="sensing" />
       <h4>Omnidirectional Sensing</h4>
       <p>
         Mavic 3 Classic continually senses objects in all directions and
@@ -182,7 +179,7 @@ const btnLeft = () => {
     <div class="cardRow">
       <div class="card">
         <div class="cardPic">
-          <img src="src/assets/images/shopInfo/controller.png" alt="" />
+          <img src="../assets/images/shopInfo/controller.png" alt="" />
         </div>
         <div class="cardInfo">
           <h5>EFVP Mavic 5</h5>
@@ -199,7 +196,7 @@ const btnLeft = () => {
     <div class="cardRow">
       <div class="card">
         <div class="cardPic">
-          <img src="src/assets/images/shopInfo/controller.png" alt="" />
+          <img src="../assets/images/shopInfo/controller.png" alt="" />
         </div>
         <div class="cardInfo">
           <h5>EFVP Mavic 3</h5>
@@ -216,7 +213,7 @@ const btnLeft = () => {
     <div class="cardRow">
       <div class="card">
         <div class="cardPic">
-          <img src="src/assets/images/shopInfo/controller.png" alt="" />
+          <img src="../assets/images/shopInfo/controller.png" alt="" />
         </div>
         <div class="cardInfo">
           <h5>EFVP Mavic 2</h5>
