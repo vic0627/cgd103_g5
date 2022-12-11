b9b7e952e2cb83cedf2106a2517690a06324d788
vic0627 <pre90719@gmail.com>
2022-12-11 20:23:14 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -123,11 +123,6 @@ const router = createRouter({
       name: "backendlogin",
       component: () => import("../views/backend/backendLogin.vue"),
     },
-    {
-      path: "/test",
-      name: "test",
-      component: () => import("../views/testBoard.vue"),
-    },
   ],
 });
 
