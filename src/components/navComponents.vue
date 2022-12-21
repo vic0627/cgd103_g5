3957ec4dd0b6adf6fecee2a84a42d620294f924e
Ramiel2209 <sa23457848551836@gmail.com>
2022-12-21 17:04:49 +0800

    nav

--- src/components/navComponents.vue
+++ src/components/navComponents.vue
@@ -23,27 +23,6 @@ const mainMenu = reactive([
   },
 ]);
 const mem = ref("");
-  {
-    id: "shop",
-    name: "STORE",
-  },
-  {
-    id: "custom",
-    name: "CUSTOM",
-  },
-  {
-    id: "about",
-    name: "ABOUT",
-  },
-  {
-    id: "race",
-    name: "RACE",
-  },
-  {
-    id: "news",
-    name: "NEWS",
-  },
-]);
 // const mem = ref("");
 onMounted(() => {
   // console.log(mainMenu);
