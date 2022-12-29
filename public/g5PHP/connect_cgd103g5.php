2dde6019bc68cf4defaa70b51e8363b5169cc13a
YiHsuan8 <ftyhnbv243@gmail.com>
2022-12-29 10:32:30 +0800

    123

--- public/g5PHP/connect_cgd103g5.php
+++ public/g5PHP/connect_cgd103g5.php
@@ -4,8 +4,8 @@ $dbname = "tibamefe_cgd103g5";
 // $password = "vwRBSb.j&K#E";
 // $port = "8888";
 $user = "root";
-//$password = "t/6vul3fu/6";
-$password = "Serfvcx265";
+$password = "t/6vul3fu/6";
+// $password = "Serfvcx265";
 $port = "3306";
 $dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";
 
